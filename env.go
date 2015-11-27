06040947af9d15fbb39e7579e0509091ec2cf0c8
Daniel Selans <dselans@newrelic.com>
2015-11-27 02:05:41 -0800

    support for []int, []string, []bool

--- env.go
+++ env.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 // ErrNotAStructPtr is returned if you pass something that is not a pointer to a
@@ -13,6 +14,14 @@ var ErrNotAStructPtr = errors.New("Expected a pointer to a Struct")
 
 // ErrUnsuportedType if the struct field type is not supported by env
 var ErrUnsuportedType = errors.New("Type is not supported")
+
+// ErrUnsupportedSliceType if the slice element type is not supported by env
+var ErrUnsuportedSliceType = errors.New("Unsupported slice type")
+
+// Friendly names for reflect types
+var sliceOfInts = reflect.TypeOf([]int(nil))
+var sliceOfStrings = reflect.TypeOf([]string(nil))
+var sliceOfBools = reflect.TypeOf([]bool(nil))
 
 // Parse parses a struct containing `env` tags and loads its values from
 // environment variables.
@@ -35,7 +44,7 @@ func doParse(ref reflect.Value, val interface{}) error {
 		if value == "" {
 			continue
 		}
-		if err := set(ref.Field(i), value); err != nil {
+		if err := set(ref.Field(i), refType.Field(i), value); err != nil {
 			return err
 		}
 	}
@@ -55,8 +64,11 @@ func getOr(key, defaultValue string) string {
 	return defaultValue
 }
 
-func set(field reflect.Value, value string) error {
+func set(field reflect.Value, refType reflect.StructField, value string) error {
 	switch field.Kind() {
+	case reflect.Slice:
+		separator := refType.Tag.Get("envSeparator")
+		return handleSlice(field, value, separator)
 	case reflect.String:
 		field.SetString(value)
 	case reflect.Bool:
@@ -75,4 +87,63 @@ func set(field reflect.Value, value string) error {
 		return ErrUnsuportedType
 	}
 	return nil
+}
+
+func handleSlice(field reflect.Value, value, separator string) error {
+	if field.Kind() != reflect.Slice {
+		return ErrUnsuportedType
+	}
+
+	if separator == "" {
+		separator = ","
+	}
+
+	splitData := strings.Split(value, separator)
+
+	switch field.Type() {
+	case sliceOfStrings:
+		field.Set(reflect.ValueOf(splitData))
+	case sliceOfInts:
+		intData, err := parseInts(splitData)
+		if err != nil {
+			return err
+		}
+		field.Set(reflect.ValueOf(intData))
+	case sliceOfBools:
+		boolData, err := parseBools(splitData)
+		if err != nil {
+			return err
+		}
+		field.Set(reflect.ValueOf(boolData))
+	default:
+		return ErrUnsuportedSliceType
+	}
+	return nil
+}
+
+func parseInts(data []string) ([]int, error) {
+	intSlice := make([]int, 0)
+
+	for _, v := range data {
+		intValue, err := strconv.ParseInt(v, 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		intSlice = append(intSlice, int(intValue))
+	}
+	return intSlice, nil
+}
+
+func parseBools(data []string) ([]bool, error) {
+	boolSlice := make([]bool, 0)
+
+	for _, v := range data {
+		bvalue, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, err
+		}
+
+		boolSlice = append(boolSlice, bvalue)
+	}
+	return boolSlice, nil
 }
