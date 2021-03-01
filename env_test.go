0b06a09b113a6a42243497ed2566baf1cbfb2afe
Ori Shoshan <ori@wing.security>
2021-03-01 13:43:07 +0200

    fix bug where using caarlos0/env on Windows under cmd.exe would incorrectly parse special cmd.exe environment variables

--- env_test.go
+++ env_test.go
@@ -1250,3 +1250,17 @@ func TestBlankKey (t *testing.T) {
 	assert.Equal(t, "", val.Blank)
 	assert.Equal(t, "", val.BlankWithTag)
 }
+
+
+// On Windows, environment variables can start with '='. This test verifies this behavior without relying on a Windows environment.
+// See env_windows.go in the Go source: https://github.com/golang/go/blob/master/src/syscall/env_windows.go#L58
+func TestToMap (t *testing.T) {
+	envVars := []string{"=::=::\\", "=C:=C:\\test", "VAR=REGULARVAR"}
+	result := toMap(envVars)
+	assert.Equal(t, map[string]string{
+		"=::": "::\\",
+		"=C:": "C:\\test",
+		"VAR": "REGULARVAR",
+	},
+	result)
+}
