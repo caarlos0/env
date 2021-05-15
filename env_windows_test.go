d001cba5c30d34f8677b5212d1eaabb602a4778a
Carlos A Becker <caarlos0@gmail.com>
2021-05-15 22:59:45 +0000

    fix: windows
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- env_windows_test.go
+++ env_windows_test.go
@@ -9,7 +9,7 @@ import (
 // On Windows, environment variables can start with '='. This test verifies this behavior without relying on a Windows environment.
 // See env_windows.go in the Go source: https://github.com/golang/go/blob/master/src/syscall/env_windows.go#L58
 func TestToMapWindows(t *testing.T) {
-	is := is.New()
+	is := is.New(t)
 	envVars := []string{"=::=::\\", "=C:=C:\\test", "VAR=REGULARVAR"}
 	result := toMap(envVars)
 	is.Equal(map[string]string{
