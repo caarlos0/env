43bff37cb4c96e52eca87f84794c74212cd6d3e7
Eng Zer Jun <engzerjun@gmail.com>
2021-05-24 22:14:10 +0800

    refactor: move from io/ioutil to os package
    The io/ioutil package has been deprecated as of Go 1.16.
    See https://golang.org/doc/go1.16#ioutil
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- env.go
+++ env.go
@@ -4,7 +4,6 @@ import (
 	"encoding"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/url"
 	"os"
 	"reflect"
@@ -299,7 +298,7 @@ func parseKeyForOption(key string) (string, []string) {
 }
 
 func getFromFile(filename string) (value string, err error) {
-	b, err := ioutil.ReadFile(filename)
+	b, err := os.ReadFile(filename)
 	return string(b), err
 }
 
