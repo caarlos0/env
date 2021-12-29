74dbb0a7d40e03d363576ef4657512fd12f15df1
Carlos A Becker <caarlos0@gmail.com>
2021-12-29 18:29:09 -0300

    feat: 0 dependencies
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- env_windows_test.go
+++ env_windows_test.go
@@ -1,18 +1,13 @@
 package env
 
-import (
-	"testing"
-
-	"github.com/matryer/is"
-)
+import "testing"
 
 // On Windows, environment variables can start with '='. This test verifies this behavior without relying on a Windows environment.
 // See env_windows.go in the Go source: https://github.com/golang/go/blob/master/src/syscall/env_windows.go#L58
 func TestToMapWindows(t *testing.T) {
-	is := is.New(t)
 	envVars := []string{"=::=::\\", "=C:=C:\\test", "VAR=REGULARVAR"}
 	result := toMap(envVars)
-	is.Equal(map[string]string{
+	isEqual(t, map[string]string{
 		"=::": "::\\",
 		"=C:": "C:\\test",
 		"VAR": "REGULARVAR",
