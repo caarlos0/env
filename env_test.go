8aee119bf3c7cc7f57c945f243c15814ca930f5b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-04-02 13:47:09 -0300

    feat: ParseAs, ParseAsWithOptions, Must (#300)
    * feat: ParseAs, ParseAsWithOptions, Must
    
    closes https://github.com/caarlos0/env/issues/299
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    * build: build with go 1.17 as well
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    * feat: change min go version to 1.18
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- env_test.go
+++ env_test.go
@@ -1854,6 +1854,46 @@ func TestGetFieldParamsError(t *testing.T) {
 	isTrue(t, errors.Is(err, NotStructPtrError{}))
 }
 
+type Conf struct {
+	Foo string `env:"FOO" envDefault:"bar"`
+}
+
+func TestParseAs(t *testing.T) {
+	config, err := ParseAs[Conf]()
+	isNoErr(t, err)
+	isEqual(t, "bar", config.Foo)
+}
+
+func TestParseAsWithOptions(t *testing.T) {
+	config, err := ParseAsWithOptions[Conf](Options{
+		Environment: map[string]string{
+			"FOO": "not bar",
+		},
+	})
+	isNoErr(t, err)
+	isEqual(t, "not bar", config.Foo)
+}
+
+type ConfRequired struct {
+	Foo string `env:"FOO,required"`
+}
+
+func TestMust(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		defer func() {
+			err := recover()
+			isErrorWithMessage(t, err.(error), `env: required environment variable "FOO" is not set`)
+		}()
+		conf := Must(ParseAs[ConfRequired]())
+		isEqual(t, "", conf.Foo)
+	})
+	t.Run("success", func(t *testing.T) {
+		t.Setenv("FOO", "bar")
+		conf := Must(ParseAs[ConfRequired]())
+		isEqual(t, "bar", conf.Foo)
+	})
+}
+
 func isTrue(tb testing.TB, b bool) {
 	tb.Helper()
 
