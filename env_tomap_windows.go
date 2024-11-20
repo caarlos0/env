6d9c7a52b0de0b45da78a3551c8307a90723584a
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-11-20 12:17:51 +0200

    refactor: simplify toMap on Windows implementation

--- env_tomap_windows.go
+++ env_tomap_windows.go
@@ -7,16 +7,10 @@ import "strings"
 func toMap(env []string) map[string]string {
 	r := map[string]string{}
 	for _, e := range env {
-		p := strings.SplitN(e, "=", 2)
-
 		// On Windows, environment variables can start with '='. If so, Split at next character.
-		// See env_windows.go in the Go source: https://github.com/golang/go/blob/master/src/syscall/env_windows.go#L58
-		prefixEqualSign := false
-		if len(e) > 0 && e[0] == '=' {
-			e = e[1:]
-			prefixEqualSign = true
-		}
-		p = strings.SplitN(e, "=", 2)
+		// See env_windows.go in the Go source: https://github.com/golang/go/blob/go1.18/src/syscall/env_windows.go#L58
+		e, prefixEqualSign := strings.CutPrefix(e, "=")
+		p := strings.SplitN(e, "=", 2)
 		if prefixEqualSign {
 			p[0] = "=" + p[0]
 		}
